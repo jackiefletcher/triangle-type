7ef1cfd8a0de1eea46cf49ab0b9d9800e6d808ff
Jackie Fletcher <jaxskriv@gmail.com>
2015-02-13 09:45:31 -0800

    will initialize files and determine if equilateral

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var triangle = function(a, b, c) {
+
+  if (a === b, b === c, c === a) {
+    return "equilateral"
+  }
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe("triangle", function() {
+  it("will determine if it is an equilateral triangle", function() {
+    expect(triangle(3, 3, 3)).to.equal("equilateral");
+  });
+
+});
