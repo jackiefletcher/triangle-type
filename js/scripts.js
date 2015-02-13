a462fd5c0445e8c10d50d76526d2c21f39be7f7d
Jackie Fletcher <jaxskriv@gmail.com>
2015-02-13 09:51:40 -0800

    will determine between equilateral, scalene and isoceles triangles

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ var triangle = function(a, b, c) {
 
   if (a === b, b === c, c === a) {
     return "equilateral"
+  } else if (a !== b && b !== c && c !== a) {
+    return "scalene"
+  } else {
+    return "isoceles"
   }
 
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,12 @@ describe("triangle", function() {
     expect(triangle(3, 3, 3)).to.equal("equilateral");
   });
 
+  it("will determine if it is a scalene triangle", function() {
+    expect(triangle(5, 7, 3)).to.equal("scalene");
+  });
+
+  it("will determine if it is an isoceles triangle", function() {
+    expect(triangle(5, 5, 3)).to.equal("isoceles");
+  });
+
 });
